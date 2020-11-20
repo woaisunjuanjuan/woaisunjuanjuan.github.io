edda8a4045a09af66a067b388f1643a13e10c143
LiuJieKun-DL <67679703+LiuJieKun-DL@users.noreply.github.com>
2020-11-19 18:29:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,12 @@
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-        "亲爱的雯雯",
-        "花开一季，有落红为证",
-        "逝去的岁月，如何能托于烟尘",
-        "你的浅笑，是我永生眷恋",
-        "相思的真情，是否能明白一二",
-        "亲爱的宝贝，你是我的唯一",
-        "爱你永远。。。",
+        "",
+        "Do not go gentle into that good night",
+        "Old age should burn and rave at close of day",
+        "Rage, rage against the dying of the light",
+        "This is Jiekun Liu's website",
+        "in memorying of our dying yesterday",
     ],
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
@@ -16,7 +15,7 @@ var config = {
     //      温馨警告:
     //          1.时分秒都必须是俩个数字  比如 00:00:00
     //          2.T不能丢了
-    date: "2017-07-20T20:15:00",
+    date: "2020-02-20T20:15:00",
     // 双方的名字
-    names: ["笨笨熊", "黑猪猪"]
+    names: ["Jiekun Liu", "UCBerkley"]
 }

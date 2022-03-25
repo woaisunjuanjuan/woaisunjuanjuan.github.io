e7f4322434be66e4b5f87933ec2558e1140118fa
woaixuhuixian <99600845+woaixuhuixian@users.noreply.github.com>
2022-03-25 15:51:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
         "",
-        "Do not go gentle into that good night",
-        "Old age should burn and rave at close of day",
-        "Rage, rage against the dying of the light 老婆我爱你",
-        "This is Jiekun Liu's website",
-        "in memorying of our dying yesterday",
+        "我知道你总是嫌弃我的年纪 会觉得“祸害我”",
+        "我知道你觉得我不够像老罗和你弟弟爱他们对象般爱你",
+        "我就是一个普通的02年的学生会点编程写点代码",
+        "我没有钱不能像他们一样给你社会地位 不能像他们一样给你很多很多...",
+        "但我用我自己的方式爱你 我对你的爱通过这个网站数字永生...谢谢你Abby 我真的很爱你！",
     ],
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
@@ -15,7 +15,7 @@ var config = {
     //      温馨警告:
     //          1.时分秒都必须是俩个数字  比如 00:00:00
     //          2.T不能丢了
-    date: "2022-03-11T07:34:00",
+    date: "2022-03-22T15:56:00",
     // 双方的名字
-    names: ["刘界锟", "徐慧娴"]
+    names: ["刘界锟", "孙娟娟"]
 }
